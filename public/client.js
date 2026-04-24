56397a60bf931b8a104d8087affd9e5bc6f3b886
Basliel Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-04-24 11:11:18 +0300

    Fix the send input event listener

--- public/client.js
+++ public/client.js
@@ -1,6 +1,9 @@
-const socket = io();
+const socket = io(); 
+
 const input = document.querySelector("#input");
+
 const send = document.querySelector("#send");
+
 const messages = document.querySelector("#messageList");
 
 send.addEventListener('click', function(){
@@ -10,11 +13,7 @@ send.addEventListener('click', function(){
     input.value= '';
   }
 });
-socket.on("Message", function(val){
-   const list = document.createElement('li');
-   list.textContent= val;
-    messages.appendChild(list);
-});
+ 
 input.addEventListener('keydown', function(e){
     if(e.key ==='Enter'){
         const message = input.value;
@@ -22,4 +21,9 @@ input.addEventListener('keydown', function(e){
         input.value= '';
     }
 
+});
+socket.on("Message", function(val){
+   const list = document.createElement('li');
+   list.textContent= val;
+    messages.appendChild(list);
 });
