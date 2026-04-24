061a0e31c868f971782fcd183cfca78434763c4d
Basliel Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-04-24 11:36:20 +0300

    Refactor server.js with time component

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
+
 const rend = express();
 const server = http.createServer(rend);
 const io = new Server(server);
 rend.use(express.static('public'));
+
 io.on('connection', function (socket){
-    console.log("Connected to with unique ID: " + socket.id);
+    const date = new Date().toLocaleTimeString();
+    console.log("[", date, "]: New client connected with ID: ", socket.id);
+    console.log("Total clients connected: ",io.engine.clientsCount);
     socket.on("Message", function (message){
-        console.log("Message received: "+ message);
+        const sent = new Date().toLocaleTimeString();
+        console.log("[", sent, "] Message received from", socket.id, ":", message);
+        console.log("Broadcasting message to ",io.engine.clientsCount," clients...");
         io.emit("Message", message);
     });
     socket.on("disconnect", function(){
-        console.log("Disconnected: ", socket.id);
+        const dis = new Date().toLocaleTimeString();
+        console.log("[", dis, "] Client disconnected: ", socket.id);
+        console.log(io.engine.clientsCount," Clients left");
     });
-
 });
 server.listen(3000, function(){
-    console.log("Connected to localhost 3000");
+    const serverTime = new Date().toLocaleTimeString();
+    console.log("[", serverTime, "] server running at http://localhost:3000");
 });
